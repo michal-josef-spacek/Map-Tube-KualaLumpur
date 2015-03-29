4e0c03fa2b9f171bcd93750fba90fcf79a0e648d
Michal Špaček <tupinek@gmail.com>
2015-03-29 11:54:50 +0200

    Remove TODO from EXAMPLE1 section in doc.

--- examples/ex1.pl
+++ examples/ex1.pl
@@ -18,4 +18,4 @@ my $route = $obj->get_shortest_route('Kuang', 'Subang Jaya');
 print "Route: ".encode_utf8($route)."\n";
 
 # Output:
-# TODO
+# Route: Kuang (Seremban Line), Sungai Buloh (Seremban Line), Kepong Sentral (Seremban Line), Kepong (Seremban Line), Segambut (Seremban Line), Putra (Port Klang Line, Walking), Bank Negara (Port Klang Line, Walking), Kuala Lumpur (Port Klang Line), KL Sentral (Port Klang Line, Terminal1, Terminal5, Walking, Terminal6), Angkasapuri (Port Klang Line), Pantai Dalam (Port Klang Line), Petaling (Port Klang Line), Jalan Templer (Port Klang Line), Kampung Dato Harun (Port Klang Line), Seri Setia (Port Klang Line), Setia Jaya (Port Klang Line), Subang Jaya (Port Klang Line)
