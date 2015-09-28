d0c34a5e71352e82ed8ae99dbf6a31fb459880ca
Michal Špaček <tupinek@gmail.com>
2015-09-28 12:16:10 +0200

    Add example for print of metro lines to doc.

--- KualaLumpur.pm
+++ KualaLumpur.pm
@@ -193,6 +193,35 @@ For more information about KualaLumpur Map, click L<here|https://https://en.wiki
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::KualaLumpur;
+
+ # Object.
+ my $obj = Map::Tube::KualaLumpur->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Ampang Line
+ # KL Monorail Line
+ # KLIA Ekspres Line
+ # KLIA Transit Line
+ # Kelana Jaya Line
+ # Port Klang Line
+ # Seremban Line
+ # Sri Petaling Line
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
