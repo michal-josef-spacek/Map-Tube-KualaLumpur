e411005541ed84dba7762333f7d7c96d1a1f5bd5
Michal Špaček <tupinek@gmail.com>
2015-11-02 11:48:47 +0100

    Add EXAMPLE5 section to doc.

--- KualaLumpur.pm
+++ KualaLumpur.pm
@@ -224,6 +224,58 @@ For more information about KualaLumpur Map, click L<here|https://https://en.wiki
  # Seremban Line
  # Sri Petaling Line
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::KualaLumpur;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = $ARGV[0];
+
+ # Object.
+ my $obj = Map::Tube::KualaLumpur->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Ampang Line' argument.
+ # Sentul Timur
+ # Sentul
+ # Titiwangsa
+ # PWTC
+ # Sultan Ismail
+ # Bandaraya
+ # Masjid Jamek
+ # Plaza Rakyat
+ # Hang Tuah
+ # Pudu
+ # Chan Sow Lin
+ # Miharja
+ # Maluri
+ # Pandan Jaya
+ # Pandan Indah
+ # Cempaka
+ # Cahaya
+ # Ampang
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
