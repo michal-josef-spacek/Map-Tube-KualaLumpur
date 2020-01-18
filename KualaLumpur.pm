6939ef98cec349ba7fe828bd180da537b052874a
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-01-18 12:00:19 +0100

    Update author github username

--- KualaLumpur.pm
+++ KualaLumpur.pm
@@ -189,8 +189,8 @@ For more information about KualaLumpur Map, click L<here|https://https://en.wiki
 
 =begin html
 
-<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-KualaLumpur/master/images/ex3.png">
-  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-KualaLumpur/master/images/ex3.png" alt="Rapid Rail" width="300px" height="300px" />
+<a href="https://raw.githubusercontent.com/michal-josef-spacek/Map-Tube-KualaLumpur/master/images/ex3.png">
+  <img src="https://raw.githubusercontent.com/michal-josef-spacek/Map-Tube-KualaLumpur/master/images/ex3.png" alt="Rapid Rail" width="300px" height="300px" />
 </a>
 
 =end html
@@ -303,7 +303,7 @@ Install the Map::Tube concrete metro modules.
 
 =head1 REPOSITORY
 
-L<https://github.com/tupinek/Map-Tube-KualaLumpur>
+L<https://github.com/michal-josef-spacek/Map-Tube-KualaLumpur>
 
 =head1 AUTHOR
 
